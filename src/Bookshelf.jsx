c3c225d44ec07000f8f4c9e543fd43a7ac4b9377
Keshaunj <keshaunjones48@gmail.com>
2024-11-01 18:55:26 -0400

    done

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -2,19 +2,72 @@ import { useState } from "react";
 import './App.css';
 
 const Bookshelf = () => {
+  
+  const [books, setBooks] = useState([
+    { title: 'PePe University', author: 'Macrus Pepe' },
+    { title: 'The Changed Man, and the Digital Law', author: 'Keshaun D jones' }
+  ]);
+
+  const [formData, setFormData] = useState({
+    title: '',
+    author: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target; 
+    setFormData((prevFormData) => ({
+      ...prevFormData,     
+      [name]: value       
+    }));
+  };
+
+  
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setBooks([...books, formData]); 
+    setFormData({ title: '', author: '' }); n
+  };
+
   return (
     <div className="bookshelfDiv">
-      <div classNmae="formDiv">
+      <div className="formDiv">
         <h3>Add a Book</h3>
-        {/*my form will be here*/}
+        <form onSubmit={handleFormSubmit}>
+          <label>
+            Title:
+            <input
+              type="text"
+              name="title"          
+              value={formData.title} 
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Author:
+            <input
+              type="text"
+              name="author"         
+              value={formData.author} 
+              onChange={handleInputChange} 
+            />
+          </label>
+          <br />
+          <button className="SubmitBttn" type="submit">Add Book</button>
+        </form>
       </div>
-        <div className="bookCardsDiv"></div>
+      <div className="bookCardsDiv">
+        <h3>Bookshelf</h3>
+        <ul>
+          {books.map((book, index) => (
+            <li key={index}>
+              <strong>{book.title}</strong> by {book.author}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
-const [books,setBooks] = useState([
-  { tilte: 'Fourth Wing', author: 'Rebecca Yarros'},
-  {title: 'The Lion, the Wicth and the Wardrobe',author: 'C.S.'}
-])
 
-export default Bookshelf
+export default Bookshelf;
